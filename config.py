2c57f5f717bd965758ff54846cd8953518f5dcbd
MOHAMED ANWAARULLAH M <128156082@sastra.ac.in>
2026-01-12 20:33:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 import os
+import sys
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-# 🔥 ONLY TWO COINS FOR TESTING
-PAIRS = ["BTCUSDT", "ETHUSDT"]
+if not BOT_TOKEN:
+    print("❌ ERROR: BOT_TOKEN environment variable is NOT set")
+    sys.exit(1)
 
+PAIRS = ["BTCUSDT", "ETHUSDT"]
 TIMEFRAME = "15m"
 
-# Pump conditions
-PRICE_PUMP_PERCENT = 1.0
-VOLUME_MULTIPLIER = 1.2
-SCAN_INTERVAL = 60
-      
+PRICE_PUMP_PERCENT = 3.0
+VOLUME_MULTIPLIER = 2.0
 
+SCAN_INTERVAL = 60 * 5
